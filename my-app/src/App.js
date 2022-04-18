73d5a8cf84b1347c7c3d88e873be84e925dda830
Gustavo Soares Thomann <gustavoprogramador18@gmail.com>
2022-04-18 03:22:21 -0300

    Primeiro contato com React JS! adicionado cabeçalho básico

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <header className='header'>
+      <h1>Gustavo</h1>
+      <ul className='menu'>
+        <li><a href=''>Sobre</a></li>
+        <li><a href=''>Comprar</a></li>
+        <li><a href=''>Contato</a></li>
+      </ul>
+    </header>
+  );
+}
+
+export default App;
